2a3bbbb34054185a7acc4f3e4c3f80f96d44e7b7
CMert Baykal <cmertbaykal@gmail.com>
2023-12-13 01:33:14 +0300

    - Demo 5

--- app/src/main/java/com/mrbaikal/composeanimationdemo/screen/demo5/Demo5.kt
+++ app/src/main/java/com/mrbaikal/composeanimationdemo/screen/demo5/Demo5.kt
@@ -1,8 +1,41 @@
 package com.mrbaikal.composeanimationdemo.screen.demo5
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Demo5() {
-
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "Demo 5",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.DarkGray.copy(alpha = 0.8f)
+        )
+        GravityLayout {
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            )
+        }
+    }
 }
